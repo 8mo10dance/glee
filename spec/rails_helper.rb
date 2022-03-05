5d1185334453056a1e05cac5cb409c369327d015
寸田智也 <dexn@xeo.local>
2022-03-06 00:02:50 +0900

    FactoryBot を設定する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,6 +62,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include FactoryBot::Syntax::Methods
+
   config.add_setting :committee_options
   config.committee_options = {
     schema_path: Rails.root.join('schema', 'schema.yml').to_s,
