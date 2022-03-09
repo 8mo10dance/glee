b24d2cfca5a7c4f0c558d00766947fe115227dd7
寸田智也 <dexn@xeo.local>
2022-03-10 03:17:55 +0900

    GET /article_groups できないのを修正する

--- app/controllers/api/v1/article_groups_controller.rb
+++ app/controllers/api/v1/article_groups_controller.rb
@@ -2,9 +2,6 @@ class Api::V1::ArticleGroupsController < ApplicationController
   def index
     article_groups = ArticleGroup
                        .joins(articles: :article_items)
-                       .merge(
-                         Article.order(published_at: :DESC)
-                       )
                        .distinct
 
     render json: { article_groups: ArticleGroupBlueprint.render_as_json(article_groups) }
